11f6b55976ee9c79592921d1acd0e4bcd8f75a7a
Ibrahim Naglah <101402062+IbrahimNaglah@users.noreply.github.com>
2023-12-13 03:05:03 +0200

    Update KEYPAD_int.h

--- KEYPAD_int.h
+++ KEYPAD_int.h
@@ -1,9 +1,4 @@
-/****************************************/
-/*          Author: Khaled Mohsen       */        
-/*          Date: 07-11-2023            */
-/*          Version: 1.0                */
-/*          Module: KEYPAD              */
-/****************************************/
+
 #ifndef KEYPAD_INT_H_
 #define KEYPAD_INT_H_
 
